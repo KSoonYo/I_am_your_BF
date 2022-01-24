5853d62ce6a21c3fcabd31aeb7c49dd78c703f5a
한승훈 <gkstmdgns422@naver.com>
2022-01-24 14:44:08 +0900

    build(frontend) : start project

---
+++ frontend/src/quasar-user-options.js
@@ -0,0 +1,17 @@
+
+import 'quasar/dist/quasar.css'
+import lang from 'quasar/lang/ko-KR.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+}
