04bfdb8b24450651c9400d1f11d3687243abb2ec
한승훈 <gkstmdgns422@naver.com>
2022-01-27 13:43:08 +0900

    build(frontend) : update quasar-user-options.js

--- frontend/src/quasar-user-options.js
+++ frontend/src/quasar-user-options.js
@@ -7,11 +7,14 @@ import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
 import '@quasar/extras/material-icons-round/material-icons-round.css'
 import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Notify, Dialog } from 'quasar'
+
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
+  config: { notify: { /* look at QuasarConfOptions from the API card */ }},
   plugins: {
+    Notify, Dialog
   },
   lang: lang
 }
