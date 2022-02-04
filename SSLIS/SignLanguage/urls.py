e8382cbcd9ebd5f2aac8e02e5e5a518bfd7a8981
gkstmdgns422 <hanseunghun422@gmail.com>
2022-02-04 17:59:57 +0900

    feat(SSLIS) : Change settings

--- SSLIS/SignLanguage/urls.py
+++ SSLIS/SignLanguage/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import result
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', result)
+    path('sslis/', include('app.urls')),
 ]
