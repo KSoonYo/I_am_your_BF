63b18cb67232614ef8251e9a2049bec5c00ee3cb
KimSoonYo <rlatnsdy125@naver.com>
2022-02-10 14:24:26 +0900

    feat(SSLIS) : add caption & sign video signal

--- frontend/src/views/conferences/components/user-video.vue
+++ frontend/src/views/conferences/components/user-video.vue
@@ -2,17 +2,14 @@
     <div v-if="streamManager">
         <ov-video :stream-manager="streamManager"/>
         <div><p>{{ clientData }}</p></div>
-		<button v-if='!onSpeech' @click='toggleOnSpeech'> 말하기 </button>
-		<button v-if='onSpeech' @click='toggleOnSpeech'> 말하기 중지 </button>
-		<div class='words'>
-		</div>
-
     </div>
 </template>
 
 <script>
 import OvVideo from './ov-video'
-import axios from 'axios'
+
+
+// const p = document.createElement('p')
 
 export default {
 	name: 'userVideo',
@@ -26,98 +23,21 @@ export default {
 		return {
 			runtimeTranscription_: "",
 			transcription_: [],
-			lang_: "ko-KR",
-			onSpeech : false,
-			baseUrl : 'http://127.0.0.1:8000/'
 		}
 	},
 	methods: {
-		startSpeechToTxt() {
-			window.SpeechRecognition =
-			window.SpeechRecognition || 
-			window.webkitSpeechRecognition
-
-			const recognition = new window.SpeechRecognition()
-			recognition.lang = this.lang_
-			recognition.interimResults = true
-			const p = document.createElement('p')
-			document.querySelector('.words').appendChild(p)
-
-			if(this.onSpeech === true){
-
-				recognition.start()
-	
-				// event current voice reco word
-				recognition.addEventListener("result", event => {      
-					var text = Array.from(event.results)
-						.map(result => result[0])
-						.map(result => result.transcript)
-						.join("");
-					this.runtimeTranscription_ = text
-					p.textContent = this.runtimeTranscription_
-				})
-	
-				// end of transcription
-				recognition.addEventListener("end", () => {											
-					axios({
-						url: this.baseUrl + 'sslis/',
-						method: 'post',
-						data: JSON.stringify(this.runtimeTranscription_)
-					})
-					.then((res) => {
-						console.log(res.data.sign)
-					})
-					
-					if( this.onSpeech === true){
-						recognition.start()
-					} else{
-						this.runtimeTranscription_ = ''
-						p.textContent = ''
-						recognition.stop()
-					}
-					
-				})
-			}
-		},
-
-		// getCookie(name){
-		// 	let cookieValue = null;
-		// 		if (document.cookie && document.cookie !== '') {
-		// 				const cookies = document.cookie.split(';');
-		// 				for (let i = 0; i < cookies.length; i++) {
-		// 						const cookie = cookies[i].trim();
-		// 						// Does this cookie string begin with the name we want?
-		// 						if (cookie.substring(0, name.length + 1) === (name + '=')) {
-		// 								cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-		// 								break;
-		// 						}
-		// 				}
-		// 		}
-		// 		return cookieValue
-		// },
-
-		toggleOnSpeech(){
-			this.onSpeech = !this.onSpeech
-		},
-
 		getConnectionData () {
-			const { connection } = this.streamManager.stream;
-			return JSON.parse(connection.data);
+			const { connection } = this.streamManager.stream
+			return JSON.parse(connection.data)
 		},
 	},
 
 	computed: {
 		clientData () {
-			const { clientData } = this.getConnectionData();
-			return clientData;
+			const { clientData } = this.getConnectionData()
+			return clientData
 		},
 	},
-
-	watch: {
-		onSpeech: function(){
-			this.startSpeechToTxt()
-		}
-	}
 
 };
 </script>
--- frontend/src/views/conferences/conference-room.vue
+++ frontend/src/views/conferences/conference-room.vue
@@ -8,33 +8,57 @@
 
       <div class="row">
         <!-- 메인 화면  -->
-        <div id="main-video col-6">
+        <!-- <div id="main-video col-6">
           <user-video :stream-manager="mainStreamManager"/>
-        </div>
+        </div> -->
 
         <!-- 참가자 화면 -->
         <div id="video-container col-6">
           <!-- host -->
-          <!-- <user-video  :stream-manager="publisher" @click="updateMainVideoStreamManager(publisher)"/> -->
+          <user-video id='publisher' :stream-manager="publisher" @click="updateMainVideoStreamManager(publisher)"/>
           
           <!-- 참가자 -->
-          <user-video class="participant" v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" @click="updateMainVideoStreamManager(sub)"/>
+          <!-- <user-video class="participant" v-for="sub in subscribers" :key="sub.stream.connection.connectionId" :stream-manager="sub" @click="updateMainVideoStreamManager(sub)"/> -->
         </div>
       </div>
 
 			<!-- 참가자 명단 -->
-			<div class="container">
+			<!-- <div class="container">
+				<h2> 참가자 명단 </h2>
 				<div v-for="subscriber in subscribers" :key="subscriber.stream.connection.connectionId">
 					<p>{{ subscriber }}</p>
 				</div>
+			</div> -->
+
+			<!-- 자막 -->
+			<div v-show='captionEnabled' class='caption'>
 			</div>
 
-			<!-- 화면공유 -->
-			<button @click="openScreen">화면공유</button>
+			<!-- 수화 video -->
+			<div v-show='videoEnabled' class='sign-video-container'>
+				<video id='videoPlayer' width='200' height='150'  muted autoplay playbackRate=3 @ended='onVideoEnded'>
+					<source :src='videoList ? videoDefaultUrl + videoList[videoIndex]: null' type='vidoe/mp4'>
+					<strong>Your browser does not support the video tag.</strong>
+				</video>
+			</div>
 
-      <!-- tool bar -->
+      <!-- tool box -->
+			<tool-box 
+			:session='session'
+			:publisher='publisher'
+			@toggleCaption='() => { captionEnabled = !captionEnabled }'
+			@toggleSignVideo='() => { videoEnabled = !videoEnabled }'
+			
+			>
+
+			</tool-box>
+
+
+
       <div id="tool-bar">
-        <button> 버튼 </button>
+				<!-- 화면공유 -->
+				<button @click="openScreen">화면공유</button>
+
       </div>
     </div>
   </div>
@@ -44,15 +68,20 @@
 import axios from 'axios'
 import { OpenVidu } from 'openvidu-browser'
 import UserVideo from './components/user-video'
+import ToolBox from './components/tool-box.vue'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json'
-const OPENVIDU_SERVER_URL =  'https://i6b107.p.ssafy.io:6443'
-const OPENVIDU_SERVER_SECRET = 'bfssafy'
+const OPENVIDU_URL = "http://" + location.hostname + ":8080";
+const VIDEO_DEFAULT_URL = 'http://127.0.0.1:8000/media/'
+
+// const OPENVIDU_SERVER_URL = 'https://' + 'i6b107.p.ssafy.io' + ':6443'
+// const OPENVIDU_SERVER_SECRET = 'bfssafy'
 
 export default {
 	name: 'session-test',
 	components: {
 		UserVideo,
+		ToolBox
 	},
 	data () {
 		return {
@@ -61,6 +90,13 @@ export default {
 			mainStreamManager: undefined,
 			publisher: undefined,
 			subscribers: [],
+			videoEnabled : false,
+			captionEnabled : false,
+			captionText : [],
+			videoList : [],
+			videoIndex : 0,
+			videoDefaultUrl : VIDEO_DEFAULT_URL,
+
 
       // conference-list에서 참여하기 버튼 눌렀을 때, 강의실 제목을 props로 전달받아야 할 듯
 			mySessionId: 'SessionA',
@@ -75,12 +111,17 @@ export default {
 			this.OV = new OpenVidu();
 			// --- Init a session ---
 			this.session = this.OV.initSession();
+
 			// --- Specify the actions when events take place in the session ---
 			// On every new Stream received...
 			this.session.on('streamCreated', ({ stream }) => {
-				const subscriber = this.session.subscribe(stream);
-				this.subscribers.push(subscriber);
-				console.log('사용자 목록 : ',this.subscribers)
+				const subscriber = this.session.subscribe(stream)
+				// if(stream.connect.data.signVideod){
+
+				// }
+
+				this.subscribers.push(subscriber)
+				// console.log('사용자 목록 : ',this.subscribers)
 			});
 			// On every Stream destroyed...
 			this.session.on('streamDestroyed', ({ stream }) => {
@@ -88,11 +129,34 @@ export default {
 				if (index >= 0) {
 					this.subscribers.splice(index, 1);
 				}
-			});
+			})
+
+			this.session.on('signal:caption', ({data})=>{
+				const p = document.createElement('p')
+				p.textContent = data
+				document.querySelector('.caption').appendChild(p)
+			})
+
+			// 비디오 소스 저장
+			this.session.on('signal:signVideo', (event)=>{
+				const videoPlayer = document.querySelector('#videoPlayer')
+				if(this.videoList.length == 0){
+					// 아예 비디오가 없는 경우
+					this.videoList = event.data.split(',')
+					videoPlayer.defaultPlaybackRate = 1
+					videoPlayer.setAttribute('src',  this.videoDefaultUrl + this.videoList[this.videoIndex])
+					videoPlayer.play()
+				}	else {
+					// 배열에 기존 비디오가 있는 경우
+					this.videoList.push.apply(this.videoList, event.data.split(','))
+				}
+			})
+
 			// On every asynchronous exception...
 			this.session.on('exception', ({ exception }) => {
-				console.warn(exception);
-			});
+				console.warn(exception)
+			})
+
 			// --- Connect to the session with a valid user token ---
 			// 'getToken' method is simulating what your server-side should do.
 			// 'token' parameter should be retrieved and returned by your own backend
@@ -100,25 +164,26 @@ export default {
 				this.session.connect(token, { clientData: this.myUserName })
 					.then(() => {
 						// --- Get your own camera stream with the desired properties ---
-						let publisher = this.OV.initPublisher(undefined, {
+						let publisher = this.OV.initPublisher('#publisher', {
 							audioSource: undefined, // The source of audio. If undefined default microphone
 							videoSource: undefined, // The source of video. If undefined default webcam
 							publishAudio: true,  	// Whether you want to start publishing with your audio unmuted or not
 							publishVideo: true,  	// Whether you want to start publishing with your video enabled or not
-							resolution: '640x480',  // The resolution of your video
+							resolution: '350x240',  // The resolution of your video
 							frameRate: 30,			// The frame rate of your video
 							insertMode: 'APPEND',	// How the video is inserted in the target element 'video-container'
 							mirror: false       	// Whether to mirror your local video or not
-						});
-						this.mainStreamManager = publisher;
-						this.publisher = publisher;
+						})
+						this.mainStreamManager = publisher
+						this.publisher = publisher
+
 						// --- Publish your stream ---
-						this.session.publish(this.publisher);
+						this.session.publish(this.publisher)
 					})
 					.catch(error => {
 						console.log('There was an error connecting to the session:', error.code, error.message);
-					});
-			});
+					})
+			})
 			window.addEventListener('beforeunload', this.leaveSession)
 		},
 		
@@ -139,7 +204,7 @@ export default {
 
 							publisher.once('accessDenied', () => {
 									console.warn('ScreenShare: Access Denied');
-							});
+							})
 
 					}).catch((error => {
 							console.warn('There was an error connecting to the session:', error.code, error.message);
@@ -150,79 +215,47 @@ export default {
 
 		leaveSession () {
 			// --- Leave the session by calling 'disconnect' method over the Session object ---
-			if (this.session) this.session.disconnect();
-			this.session = undefined;
-			this.mainStreamManager = undefined;
-			this.publisher = undefined;
-			this.subscribers = [];
-			this.OV = undefined;
-			window.removeEventListener('beforeunload', this.leaveSession);
-
-      // ---- conference-list로 push  ----
-
+			if (this.session) this.session.disconnect()
+			this.session = undefined
+			this.mainStreamManager = undefined
+			this.publisher = undefined
+			this.subscribers = []
+			this.OV = undefined
+			window.removeEventListener('beforeunload', this.leaveSession)
 
 		},
 		updateMainVideoStreamManager (stream) {
 			if (this.mainStreamManager === stream) return;
 			this.mainStreamManager = stream;
 		},
-		/**
-		 * --------------------------
-		 * SERVER-SIDE RESPONSIBILITY
-		 * --------------------------
-		 * These methods retrieve the mandatory user token from OpenVidu Server.
-		 * This behavior MUST BE IN YOUR SERVER-SIDE IN PRODUCTION (by using
-		 * the API REST, openvidu-java-client or openvidu-node-client):
-		 *   1) Initialize a Session in OpenVidu Server	(POST /openvidu/api/sessions)
-		 *   2) Create a Connection in OpenVidu Server (POST /openvidu/api/sessions/<SESSION_ID>/connection)
-		 *   3) The Connection.token must be consumed in Session.connect() method
-		 */
+
+
+		onVideoEnded(){
+			const videoPlayer = document.querySelector('#videoPlayer')
+			if(this.videoIndex < this.videoList.length - 1){
+				this.videoIndex++
+			} else{
+				this.videoIndex = 0
+				this.videoList = []
+				return
+			}
+			videoPlayer.defaultPlaybackRate = 1
+			videoPlayer.setAttribute('src', this.videoDefaultUrl + this.videoList[this.videoIndex])
+			videoPlayer.play()
+		},
+
+
 		getToken (mySessionId) {
-			return this.createSession(mySessionId).then(sessionId => this.createToken(sessionId));
-		},
-		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-session
-		createSession (sessionId) {
-      // ----- backend token api 문서 참고해서 url 수정 -----
 			return new Promise((resolve, reject) => {
-				axios
-					.post(`${OPENVIDU_SERVER_URL}/openvidu/api/sessions`, JSON.stringify({
-						customSessionId: sessionId,
-					}), {
-						auth: {
-							username: 'OPENVIDUAPP',
-							password: OPENVIDU_SERVER_SECRET,
-						},
-					})
-					.then(response => response.data)
-					.then(data => resolve(data.id))
-					.catch(error => {
-						if (error.response.status === 409) {
-							resolve(sessionId);
-						} else {
-							console.warn(`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}`);
-							if (window.confirm(`No connection to OpenVidu Server. This may be a certificate error at ${OPENVIDU_SERVER_URL}\n\nClick OK to navigate and accept it. If no certificate warning is shown, then check that your OpenVidu Server is up and running at "${OPENVIDU_SERVER_URL}"`)) {
-								location.assign(`${OPENVIDU_SERVER_URL}/accept-certificate`);
-							}
-							reject(error.response);
-						}
-					});
+					axios.post(`${OPENVIDU_URL}/api/openvidu/get-token`, JSON.stringify({
+									sessionName: mySessionId,
+							}))
+							.then(response => response.data)
+							.then(data => resolve(data.token))
+							.catch(error => reject(error.response));
 			});
-		},
-		// See https://docs.openvidu.io/en/stable/reference-docs/REST-API/#post-connection
-		createToken (sessionId) {
-			return new Promise((resolve, reject) => {
-				axios
-					.post(`${OPENVIDU_SERVER_URL}/openvidu/api/sessions/${sessionId}/connection`, {}, {
-						auth: {
-							username: 'OPENVIDUAPP',
-							password: OPENVIDU_SERVER_SECRET,
-						},
-					})
-					.then(response => response.data)
-					.then(data => resolve(data.token))
-					.catch(error => reject(error.response));
-			});
-		},
+	},
+
 	},
 
   created(){
