384b3423f778db08e64064bfcc10a465ea336493
손모은 <mo_ah@naver.com>
2022-02-11 15:39:42 +0900

    Design(frontend) : quasarUserOption

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { Quasar } from 'quasar'
-// import quasarUserOptions from './quasar-user-options'
+import quasarUserOptions from './quasar-user-options'
 import axios from 'axios'
 
 
   
 const app = createApp(App)
 app.config.globalProperties.axios = axios;
-app.use(Quasar).use(store).use(router).mount('#app')
+app.use(Quasar,quasarUserOptions).use(store).use(router).mount('#app')
 
 axios.defaults.baseURL = 'http://localhost:8080'
