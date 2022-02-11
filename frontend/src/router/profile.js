6ea1f3dd8ed3d4f5de42f2e9ffc6c4f7a75856fd
한승훈 <gkstmdgns422@naver.com>
2022-02-11 18:47:07 +0900

    feat(frontend) : add profile-update

--- frontend/src/router/profile.js
+++ frontend/src/router/profile.js
@@ -1,5 +1,4 @@
 import profile from '@/views/profile/profile'
-import profileUpdate from '@/views/profile/profile-update'
 
 
 export default [
@@ -7,12 +6,5 @@ export default [
     path: '/profile',
     name: 'profile',
     component: profile,
-    children: [
-      {
-        path: '/lobby/profile-update/',
-        name: 'profileUpdate',
-        component: profileUpdate
-      }
-    ]
   },
 ]
